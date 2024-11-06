1ecad65eba6e52d1eff83f1c15072f77244b6381
EmreAnliacik <emreanliacik@hotmail.com>
2024-11-06 14:07:32 +0100

    Annotation-priority

--- src/Gun01/_01_Giris.java
+++ src/Gun01/_01_Giris.java
@@ -1,7 +1,35 @@
 package Gun01;
 
+import org.testng.annotations.Test;
+
 public class _01_Giris {
-    public static void main(String[] args) {
-        System.out.println("Merhaba");
+
+    //default olarak alfabetik siraya gore calisir.
+
+
+    @Test(priority = 1)
+    public void driverIslemleriniYap() {
+        System.out.println("Driver Islemleri yapildi");
+
     }
+
+    @Test(priority = 2)
+    public void WebSitesiniAc() {
+        System.out.println("Web sitesi acildi");
+
+    }
+
+    @Test(priority = 3)
+    public void loginTestiYap() {
+        System.out.println("Login Testi yapildi");
+
+    }
+
+    @Test(priority = 4)
+    public void driverKapat() {
+        System.out.println("Driver Kapatildi");
+
+    }
+
+
 }
