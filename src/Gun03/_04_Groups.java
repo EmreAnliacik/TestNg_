add038bb3fc72c0d4b4062775c1d0f46aa5063c3
EmreAnliacik <emreanliacik@hotmail.com>
2024-11-09 12:46:17 +0100

    TestNG-xml

---
+++ src/Gun03/_04_Groups.java
@@ -0,0 +1,44 @@
+package Gun03;
+
+import org.testng.annotations.Test;
+
+public class _04_Groups {
+
+    @Test(groups = {"Smoke Test"})
+    public void Test1(){
+        System.out.println("Test1");
+    }
+
+    @Test(groups = {"Smoke Test"})
+    public void Test3(){
+        System.out.println("Test3");
+    }
+
+    @Test(groups = {"Smoke Test"})
+    public void Test5(){
+        System.out.println("Test5");
+    }
+
+    @Test(groups = {"Regression"})
+    public void Test2(){
+        System.out.println("Test2");
+    }
+
+    @Test(groups = {"Regression"})
+    public void Test4(){
+        System.out.println("Test4");
+    }
+
+    @Test(groups = {"Regression"})
+    public void Test6(){
+        System.out.println("Test6");
+    }
+
+    @Test
+    public void Test7(){
+        System.out.println("Test7");
+    }
+
+
+
+}
