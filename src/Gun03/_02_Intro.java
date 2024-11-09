a60bf53878d0265e550086f89d282c6d1223f5e9
EmreAnliacik <emreanliacik@hotmail.com>
2024-11-09 12:00:29 +0100

    TestNG-xml

--- src/Gun03/_02_Intro.java
+++ src/Gun03/_02_Intro.java
@@ -1,4 +1,89 @@
 package Gun03;
 
+import org.testng.annotations.*;
+
 public class _02_Intro {
-}
+
+    /*
+      @BeforeSuit
+        @BeforeTest -> içinde çok metod,group,class olan test Grubu
+          @BeforeGroup
+            @BeforeClass
+              @BeforeMetod
+                @Test test1()  test metodu
+              @AfterMetod
+            @AfterClass
+          @AfterGroup
+        @AfterTest
+      @AfterSuit
+     */
+
+
+
+        @BeforeSuite
+        public void bSuit() {
+            System.out.println("Before Suit");
+        }
+
+        @BeforeTest
+        public void bTest() {
+            System.out.println("Before Test");
+        }
+
+        @BeforeGroups
+        public void bGroup() {
+            System.out.println("Before Grouo");
+        }
+
+        @BeforeClass
+        public void bClass() {
+            System.out.println("Before Class");
+        }
+
+        @BeforeMethod
+        public void bMethod() {
+            System.out.println("Before Method");
+        }
+
+
+        @Test
+        public void Intro1_Test1() {
+            System.out.println("Intro2_Test1");
+        }
+
+        @Test
+        public void Intro1_Test2() {
+            System.out.println("Intro2_Test2");
+        }
+
+        @AfterMethod
+        public void aMethod() {
+            System.out.println("After Method");
+        }
+
+
+        @AfterClass
+        public void aClass() {
+            System.out.println("After Class");
+        }
+
+
+        @AfterGroups
+        public void aGroup() {
+            System.out.println("After Group");
+        }
+
+
+        @AfterTest
+        public void aTest() {
+            System.out.println("After Test");
+        }
+
+        @AfterSuite
+        public void aSuit() {
+            System.out.println("After Suit");
+        }
+
+
+    }
+
---
+++ src/Gun03/_03_Intro.java
@@ -0,0 +1,20 @@
+package Gun03;
+
+import org.testng.annotations.Test;
+
+public class _03_Intro {
+
+
+
+    @Test
+    public void Intro1_Test1(){
+        System.out.println("Intro3_Test1");
+    }
+
+    @Test
+    public void Intro1_Test2(){
+        System.out.println("Intro3_Test2");
+    }
+
+
+}
